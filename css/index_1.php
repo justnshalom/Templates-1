a150f7d7a9e15a50c3f9894a3562587898ec07f8
David A Lopez <david.cadc@gmail.com>
2013-10-08 14:10:15 -0500

    subiendo archivos

---
+++ css/index_1.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head  lang="es">
+        <meta charset="utf-8">
+        <meta  name="author" content="Juan2ramos" />
+        <meta  name="description" content="Pagina inicial" />
+        <meta name="viewport" content="width=device-width, initial-scale= 1.0 maximum-scale=1" />
+
+        <!-- CSS -->
+        <link rel="stylesheet" href="css/normalize.css" />
+        <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/style.css" />
+
+
+        <title>Pagina inicial</title>
+    </head>
+    <body>
+        <header>
+            <button id="der"></button>
+            <button id="izq"></button>
+
+        </header>
+        <section id="contacto">
+            <div id="wrapper">  
+                <p>Vous cherchez un nouveau membre à votre équipe de héros ou un allié fiable pour une bataille épique ? Ne cherchez plus !</p>
+                <button>Contactame</button>
+            </div>
+
+        </section>
+        <section id="images">
+            <h3>Mis proyectos</h3>
+
+        </section>
+        <footer>
+
+        </footer>
+
+
+    </body>
+</html>
