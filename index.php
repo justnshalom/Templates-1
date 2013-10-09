26affbbfff93d797e048874859cd110a9201a20f
David A Lopez <david.cadc@gmail.com>
2013-10-09 13:43:27 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <meta  name="description" content="Pagina inicial" />
         <meta name="viewport" content="width=device-width, initial-scale= 1.0 maximum-scale=1" />
 
-        <!-- CSS -->
+        <!-- CSS ooooooo -->
         <link rel="stylesheet" href="css/normalize.css" />
         <link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900,300italic,400italic,700italic,900italic' rel='stylesheet' type='text/css'>
         <link rel="stylesheet" href="css/style.css" />
