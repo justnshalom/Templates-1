5c03f85c628e02983a1ee18c3bbac220855d2ddf
davidlopez <david.cadc@gmail.com>
2013-10-16 16:26:20 -0500

    otro commit

--- css/index_1.php
+++ css/index_1.php
@@ -30,7 +30,7 @@
         <section id="images">
             <h3>Mis proyectos</h3>
             <h3>Mis proyectos</h3>
-            <p class="title">Projet ARUC en relations industrielleoooooooooooooaaaaa </p>
+            <p class="title">Projet ARUC en relations industriell </p>
 
         </section>
         <footer>
